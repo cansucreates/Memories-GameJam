a059061f5074351b3e51f1b98bca98fa034a8c77
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2025-02-22 17:55:38 +0300

    added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.right * moveX + transform.forward * moveZ;
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
